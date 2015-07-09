114c12640994848ab92aad7422a4d10684a8b3c9
Lucas Fernando Amorim <lf.amorim@bipbop.com.br>
2015-07-09 13:56:40 -0300

    feature: menu item with material design

--- src/js/internals/interface/menu.js
+++ src/js/internals/interface/menu.js
@@ -1,23 +1,34 @@
-/* global module */
+/* global module, componentHandler */
 
 
 var menu = function () {
 
     this.add = function (title, icon) {
+        var elementId = "action-" + title;
         var elementIcon = $("<i />").addClass("fa fa-" + icon);
         var elementLink = $("<a />").attr({
-            title: title,
-            href: "#"
+            href: "#",
+            id: elementId
         });
-        var elementItem = $("<li />");
 
-        elementItem.append(elementLink.append(elementIcon));
+        var elementItem = $("<li />");
+        var elementTooltip = $("<div />").attr({
+            class: "mdl-tooltip",
+            "for": elementId
+        }).text(title);
+
+        elementItem
+                .append(elementLink.append(elementIcon))
+                .append(elementTooltip);
 
         $(".module-menu").append(elementItem);
 
+        componentHandler.upgradeElement(elementTooltip.get(0), "MaterialTooltip");
+        
         return {
             nodeLink: elementLink,
-            nodeItem: elementItem
+            nodeItem: elementItem,
+            nodeTooltip: elementTooltip
         };
     };
 
