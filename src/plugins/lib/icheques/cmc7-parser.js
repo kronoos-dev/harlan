98d0cf30912b81a382a62a81f51f369aaf50ef3e
Lucas Fernando Amorim <lf.amorim@bipbop.com.br>
2016-04-16 23:55:25 -0300

    fast llvm

--- src/plugins/lib/icheques/cmc7-parser.js
+++ src/plugins/lib/icheques/cmc7-parser.js
@@ -1,15 +1,16 @@
-const CMC7_REGEX = /^(\d{3})(\d{4})\d(\d{3})(\d{6})\d{13}$/;
-
+const CMC7_REGEX = /^(\d{3})(\d{4})\d(\d{3})(\d{6})\d{13}$/,
+    NON_NUMBERS = /[^\d]/;
+    
 export class CMC7Parser {
-  constructor(code) {
-      let execution = CMC7_REGEX.exec(code.replace(/[^\d]/, ''));
-      if (!execution) {
-          throw "Could not interpret the check code.";
-      }
-      this.bank = execution[1];
-      this.agency = execution[2];
-      this.compensation = execution[3];
-      this.number = execution[4];
-  }
+    constructor(code) {
+        let execution = CMC7_REGEX.exec(code.replace(NON_NUMBERS, ''));
+        if (!execution) {
+            throw "Could not interpret the check code.";
+        }
+        this.bank = execution[1];
+        this.agency = execution[2];
+        this.compensation = execution[3];
+        this.number = execution[4];
+    }
 
 }
