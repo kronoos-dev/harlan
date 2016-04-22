440c687918e9f468f52c3dfac1feebabbf6776f3
Joao Paulo Barbosa <jpaulobneto@gmail.com>
2016-04-22 14:32:35 -0300

    Gerando o Header do .ban

--- src/plugins/lib/icheques/ban-factory.js
+++ src/plugins/lib/icheques/ban-factory.js
@@ -1,6 +1,7 @@
-import jDataView from "jdataview";
+import jDataView from 'jdataview';
 
-const NON_NUMERIC = /[^\d]/g,
+const NON_NUMERIC = /[\D]/g,
+      NON_WORD = /[\PL]/g,
       ROW_SIZE = 502,
       BAN_VERSION = '02.7',
       CRLF = '\r\n';
@@ -22,7 +23,7 @@ export class BANFactory {
     }
 
     _fillBuffer() {
-        this.buffer.setString(0, new Array(this.size).join(" "));
+        this.buffer.setString(0, new Array(this.size).join(' '));
         for (var i = 0; i < this.size; i += ROW_SIZE) {
             this.buffer.setString(i + ROW_SIZE - CRLF.length, CRLF);
         }
@@ -46,9 +47,43 @@ export class BANFactory {
         let doc = this.company.cnpj || this.company.cpf;
 
         this.buffer.setString(0, '1');
+        // CPF/CNPJ. de 2 a 15. 14.
         this.buffer.setString(1, doc.replace(NON_NUMERIC, '').substring(0, 14));
-        this.buffer.setString(15, this.company.cnpj ? "1" : "2");
+        // T:CPF/CNPJ. de 16 a 16. 1.
+        this.buffer.setString(15, this.company.cnpj ? '1' : '2');
+        // Nome do Cedente. de 17 a 56. 40.
+        this.buffer.setString(16, this.company.nome.replace(NON_WORD, ' ').substring(0, 40));
+        // Endereco. de 57 a 96. 40.
+        var endereco = [
+            this.company.endereco[0],
+            this.company.endereco[1],
+            this.company.endereco[2]
+        ].join(' ');
+        console.log(endereco);
+        this.buffer.setString(56, endereco.substring(0, 40));
+        // Cidade. de 97 a 114. 18.
+        this.buffer.setString(96, this.company.endereco[5].substring(0, 18));
+        // Estado. de 115 a 116. 2.
+        this.buffer.setString(114, this.company.endereco[6].substring(0, 2));
+        // CEP. de 117 a 124. 8.
+        this.buffer.setString(116, this.company.endereco[4].replace(NON_NUMERIC, '').substring(0, 8));
+        // Data de operação. de 180 a 185. 6.
         this.buffer.setString(179, moment().format('DDMMYY'));
+        // Fator da operação. de 186 a 192. 7.
+        this.buffer.setString(185, '0000000');
+        // Tx efetiva min. de 193 a 199. 7.
+        this.buffer.setString(192, '0000000');
+        // Tx serviço. de 200 a 206. 7.
+        this.buffer.setString(199, '0000000');
+        // Tx ISS. de 207 a 213. 7.
+        this.buffer.setString(206, '0000000');
+        // Tarifa por título. de 214 a 220. 7.
+        this.buffer.setString(213, '0000000');
+        // Tarifa por cheque. de 251 até 257. 7.
+        this.buffer.setString(250, '0000000');
+        // Tx Serviço Trustee. de 265 até 271. 7.
+        this.buffer.setString(264, '0000000');
+        // 02.7. de 397 a 400. 4.
         this.buffer.setString(396, BAN_VERSION);
     }
 
