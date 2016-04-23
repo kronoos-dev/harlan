acc2b074a3ed7713f003f1379c8bb1de1b68b94e
Lucas Fernando Amorim <lf.amorim@bipbop.com.br>
2016-04-23 20:56:48 -0300

    regula o valor do cheque em inteiro apos receber da interface, campo de nome e CPF para localizacao do cheque

--- src/plugins/lib/icheques/edit.js
+++ src/plugins/lib/icheques/edit.js
@@ -14,30 +14,41 @@ module.exports = function(controller) {
             controller.call("credits::has", 30, () => {
                 controller.serverCommunication.call("UPDATE 'ICHEQUES'.'ONEMONTH'",
                     controller.call("error::ajax", controller.call("loader::ajax", {
-                    data: check,
-                    success: () => {
-                        /* websocket updates =p */
-                        toastr.success("Um mês adicionado ao vencimento com sucesso.", "Dados atualizados com sucesso.");
-                    }
-                }, true)));
+                        data: check,
+                        success: () => {
+                            /* websocket updates =p */
+                            toastr.success("Um mês adicionado ao vencimento com sucesso.", "Dados atualizados com sucesso.");
+                        }
+                    }, true)));
             });
         });
     });
 
-    controller.registerCall("icheques::item::edit", function(check) {
+    controller.registerCall("icheques::item::edit", function(check, callback, optionalAmmount = true) {
         var cmc7Data = new CMC7Parser(check.cmc),
             form = controller.call("form", (parameters) => {
                 parameters.cmc = check.cmc;
+                parameters.ammount = Math.floor(parameters.ammount * 100);
                 controller.call("confirm", {}, () => {
                     controller.serverCommunication.call("UPDATE 'ICHEQUES'.'CHECKDATA'",
                         controller.call("error::ajax", controller.call("loader::ajax", {
                             data: parameters,
+                            error: function() {
+                                if (callback) callback("ajax failed", check);
+                            },
                             success: () => {
                                 /* websocket updates =p */
+                                check.ammount = parameters.ammount;
+                                check.observation = parameters.observation;
                                 toastr.success("Os dados do cheque foram atualizados com sucesso.", "Dados atualizados com sucesso.");
+                                if (callback) callback(null, check);
                             }
                         }, true)));
+                }, () => {
+                    controller.call("icheques::item::edit", check, callback, optionalAmmount);
                 });
+            }, () => {
+                if (callback) callback("can't edit", check);
             });
 
         form.configure({
@@ -50,11 +61,28 @@ module.exports = function(controller) {
                 magicLabel: true,
                 fields: [
                     [{
+                        name: "document",
+                        type: "text",
+                        placeholder: "Documento",
+                        labelText: "Documento",
+                        disabled: true,
+                        optional: true,
+                        value: check.cpf || check.cnpj,
+                    }, {
+                        name: "name",
+                        type: "text",
+                        placeholder: "Nome",
+                        labelText: "Nome do Titular",
+                        optional: true,
+                        disabled: true
+                    }],
+                    [{
                         name: "check-number",
                         type: "text",
                         placeholder: "Número do Cheque",
                         labelText: "Número do Cheque",
                         disabled: true,
+                        optional: true,
                         value: cmc7Data.number,
                     }, {
                         name: "ammount",
@@ -63,7 +91,7 @@ module.exports = function(controller) {
                         labelText: "Valor do Cheque",
                         value: check.ammount,
                         mask: "000.000.000.000,00",
-                        required: false,
+                        optional: optionalAmmount,
                         maskOptions: {
                             reverse: true
                         },
@@ -74,11 +102,26 @@ module.exports = function(controller) {
                         type: "textarea",
                         placeholder: "Observação",
                         labelText: "Observação",
-                        required: false,
+                        optional: true,
                         value: check.observation
                     }
                 ]
             }]
         });
+
+        controller.server.call("SELECT FROM 'BIPBOPJS'.'CPFCNPJ'", {
+            data: {
+                documento: check.cpf || check.cnpj
+            },
+            success: (ret) => {
+                form.setValue("name", $("BPQL > body nome", ret).text());
+            }
+        });
+
     });
+
+    controller.registerCall("icheques::item::setAmmount", function(check, callback) {
+        controller.call("icheques::item::edit", check, callback, false);
+    });
+
 };
