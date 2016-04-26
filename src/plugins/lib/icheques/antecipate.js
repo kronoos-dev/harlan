c6d847bbeaf3345c2c67c41252c37f3fbc0e99d8
Lucas Fernando Amorim <lf.amorim@bipbop.com.br>
2016-04-26 18:10:24 -0300

    modais de antecipacao

--- src/plugins/lib/icheques/antecipate.js
+++ src/plugins/lib/icheques/antecipate.js
@@ -1,7 +1,6 @@
 import _ from 'underscore';
-import {
-    queue
-} from 'async';
+import { queue } from 'async';
+import { titleCase } from 'change-case';
 
 /* global module, numeral */
 
@@ -25,13 +24,13 @@ module.exports = function(controller) {
                 var q = queue(controller.reference("icheques::item::setAmmount"), 1);
 
                 q.push(noAmmountChecks, (err) => {
-                    if (err) {
-                        q.kill();
-                    }
+                    /* pass */
                 });
 
                 q.drain = () => {
-                    controller.call("icheques::antecipate", checks);
+                    controller.call("icheques::antecipate", _.filter(checks, (obj) => {
+                         return obj.ammount;
+                    }));
                 };
 
                 controller.call("icheques::show", noAmmountChecks);
@@ -40,10 +39,10 @@ module.exports = function(controller) {
             return;
         }
 
-        controller.serverCommunication.call("SELECT FROM 'ICHEQUESBANK'.'BANKS'",
+        controller.serverCommunication.call("SELECT FROM 'ICHEQUESFIDC'.'LIST'",
             controller.call("loader::ajax", controller.call("error::ajax", {
                 data: {
-                    filter: "icheques"
+                    approved: "true"
                 },
                 success: function(ret) {
                     controller.call("icheques::antecipate::show", ret, checks);
@@ -53,7 +52,7 @@ module.exports = function(controller) {
 
 
     controller.registerCall("icheques::antecipate::show", function(data, checks) {
-        var banks = $("BPQL > body > banks > node", data);
+        var banks = $("BPQL > body > fidc", data);
 
         if (!banks.length) {
             controller.call("alert", {
@@ -73,42 +72,106 @@ module.exports = function(controller) {
 
         banks.each(function(i, element) {
             list.add("fa-share", [
-                $("name", element).text(),
-                $("bio", element).text(),
-                $("cep", element).text(),
-                $("address address:item(0)", element).text(),
-                $("address address:item(0)", element).text(),
+                $("company > nome", element).text() || $("company > responsavel", element).text() || $("company > username", element).text(),
+                `${$("company > endereco > node:eq(4)", element).text()} / ${$("company > endereco > node:eq(6)", element).text()}`,
+                $(element).children("bio").text(),
             ]).click(function(e) {
-                controller.call("confirm", {
-                    title: "Você deseja realmente antecipar estes títulos?",
-                    subtitle: "Será enviado um e-mail para factoring solicitando aprovação.",
-                    paragraph: "Os cheques não poderão serem processados novamente, você deverá aguardar a aprovação ou rejeição. A factoring reserva o direito de aceitar apenas alguns títulos de sua carteira."
-                }, function() {
-                    e.preventDefault();
-                    controller.serverCommunication.call("INSERT INTO 'ICHEQUES'.'ANTECIPATE'",
-                        controller.call("error::ajax", controller.call("loader::ajax", {
-                            method: "post",
-                            data: {
-                                factoring: $("id", element),
-                                checks: _.pluck(checks, "cmc").join(",")
-                            },
-                            success: function() {
-                                controller.call("alert", {
-                                    icon: "pass",
-                                    title: "Seus cheques foram enviados",
-                                    subtitle: "Aguarde a resposta do fundo em sua caixa eletrônica"
-                                });
-                            }
-                        })));
-                });
+                controller.call("icheques::antecipate::fidc", data, element, checks);
+                modal.close();
             });
+        });
+
+        list.element().find("li:first div:last").css({
+            width: "250px"
         });
 
         modal.createActions().add("Sair").click(function(e) {
             e.preventDefault();
             modal.close();
         });
+    });
 
+    var companyData = (paragraph, element) => {
+        var phones = $("<ul />").addClass("phones");
+        $("company > telefone > node", element).each((idx, node) => {
+            var get = (idx) => {
+                return $(`node:eq(${idx.toString()})`, node).text();
+            };
+
+            var phoneNumber = `Telefone: (${get(0)}) ${get(1)}${get(2).length ? "#" + get(2) : ""} - ${titleCase(get(4))}`;
+            phones.append($("<li />").text(phoneNumber));
+        });
+
+        var emails = $("<ul />").addClass("emails");
+        $("company > email > node", element).each((idx, node) => {
+            var emailAddress = `E-mail: ${$("node:eq(0)", node).text()} - ${titleCase($("node:eq(1)", node).text())}`;
+            emails.append($("<li />").text(emailAddress));
+        });
+
+        var get = (idx) => {
+            return $(`endereco node:eq(${idx.toString()})`, element).text();
+        };
+
+        var address = `${get(0)} ${get(1)} ${get(2)} ${get(3)} - ${get(5)} ${get(4)} ${get(6)} `;
+
+        var addressNode = $("<p />").text(address).addClass("address").append($("<a />").attr({
+            href: `http\:\/\/maps.google.com\?q\=${encodeURI(address)}`,
+            target: '_blank'
+        }).append(
+            $("<div />").addClass("map").css({
+                "background-image": `url(http\:\/\/maps.googleapis.com/maps/api/staticmap?center=${encodeURI(address)}&zoom=13&scale=false&size=600x200&maptype=roadmap&format=png&visual_refresh=true)`
+            })
+        ));
+
+        paragraph.append(emails).append(phones).append(addressNode);
+        return [emails, phones, addressNode];
+    };
+
+    controller.registerCall("icheques::antecipate::fidc", (data, element, checks) => {
+        var modal = controller.modal();
+        modal.gamification("sword").css({
+            "background": `url(${$(element).children("logo").text()}) no-repeat center`
+        });
+        modal.title($("company > nome", element).text() || $("company > responsavel", element).text());
+        modal.subtitle($("company > cnpj", element).text() || $("company > cpf", element).text());
+        var paragraph = modal.paragraph($(element).children("bio").text());
+
+        companyData(paragraph, element);
+
+        var form = modal.createForm();
+        form.addSubmit("send", "Antecipar");
+        form.element().submit((e) => {
+            e.preventDefault();
+            modal.close();
+            controller.call("confirm", {
+                title: "Você deseja realmente antecipar estes títulos?",
+                subtitle: "Será enviado um e-mail para factoring solicitando aprovação.",
+                paragraph: "Os cheques não poderão serem processados novamente, você deverá aguardar a aprovação ou rejeição. A factoring reserva o direito de aceitar apenas alguns títulos de sua carteira."
+            }, () => {
+                e.preventDefault();
+                controller.serverCommunication.call("INSERT INTO 'ICHEQUES'.'ANTECIPATE'",
+                    controller.call("error::ajax", controller.call("loader::ajax", {
+                        method: "post",
+                        data: {
+                            factoring: $("id", element),
+                            checks: _.pluck(checks, "cmc").join(",")
+                        },
+                        success: function() {
+                            controller.call("alert", {
+                                icon: "pass",
+                                title: "Seus cheques foram enviados",
+                                subtitle: "Aguarde a resposta do fundo em sua caixa eletrônica"
+                            });
+                        }
+                    })));
+            }, () => {
+                controller.call("icheques::antecipate::show", data, checks);
+            });
+        });
+
+        modal.createActions().cancel(() => {
+            controller.call("icheques::antecipate::show", data, checks);
+        });
     });
 
 };
