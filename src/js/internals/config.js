08a09dea61dc4dbb48eca432fc2e569dfd2d5876
Lucas Fernando Amorim <lf.amorim@bipbop.com.br>
2016-05-02 18:49:33 -0300

    voltando com o arquivo de configuracao

---
+++ src/js/internals/config.js
@@ -0,0 +1,49 @@
+module.exports = {
+    // websocketAddress: "ws://localhost:8090/",
+    // webserviceAddress: "http://localhost:8080/",
+    proshield: {
+        hosts: [
+            "proshield",
+            "proshield.harlan.com.br"
+        ]
+    },
+    kronoos: {
+        endpoint: "https://js.kronoos.com/index.js",
+        hosts: [
+            "kronoos",
+            "kronoos.com",
+            "www.kronoos.com",
+            "kronoos.com.br",
+            "www.kronoos.com.br"
+        ]
+    },
+    dive: {
+        endpoint: "https://js.dive.harlan.com.br/index.js",
+        hosts: [
+            "dive",
+            "dive.harlan.com.br"
+        ]
+    },
+    icheques: {
+        hosts: [
+            "icheques",
+            "painel.icheques.com.br"
+        ]
+    },
+    instantSearchDelay: 500, /* ms */
+    animatedShowTable: 300,
+    hideAutocomplete: 300,
+    iugu: {
+        token: "44176a3c-50ec-4c45-b092-1d957813d22d"
+    },
+    oauthKey: "AYY0iBNDo95aIcw--iWIqa71ZJs",
+    checkoutUrl: 'https://irql.bipbop.com.br/api/checkout.html',
+    inboxTime: 50000,
+    googleAnalyticsId: "UA-36688252-3", /* Universal Analytics */
+    web3: {
+        endpoint: "http://localhost:8545"
+    },
+    gcm: {
+        apiKey : "AIzaSyAwitAYDKWMC4WYfF4YW5pTVN_GS1yxa-8"
+    }
+};
