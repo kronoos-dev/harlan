292e89db62215dcd841a6af97fd80ec7e915e4e4
Lucas Fernando Amorim <lf.amorim@bipbop.com.br>
2016-05-07 09:55:16 -0300

    working mode

--- src/plugins/application-statistics.js
+++ src/plugins/application-statistics.js
@@ -1,44 +1,53 @@
-/* global harlan, numeral */
+/* global controller. numeral */
 
-(function (controller) {
+(function(controller) {
 
-    var moreInformation = function (node, result) {
-        return function (e) {
+    var moreInformation = function(node, result) {
+        return function(e) {
             e.preventDefault();
 
             var name = $(node).find("_id > name").text();
 
             result.addSeparator("Estatísticas para a Fonte " + name.toUpperCase(),
-                    "Latência e disponibilidade",
-                    "Informações do Teste de Integração");
+                "Latência e disponibilidade",
+                "Informações do Teste de Integração");
             result.addItem("Latência", numeral(parseInt($(node).find("averageResponseTime").text(), 10) / 1000).format("0") + " segs").addClass("center");
             result.addItem("Latência Máxima", numeral(parseInt($(node).find("maxResponseTime").text(), 10) / 1000).format("0") + " segs").addClass("center");
 
         };
     };
 
-    var parserConsultas = function (document) {
-        var result = harlan.call("result");
+    var parserConsultas = function(document) {
+        var result = controller.call("result");
         var graph = [];
 
         var jdocument = $(document);
-        jdocument.find("result > node").each(function (idx, node) {
+        jdocument.find("result > node").each(function(idx, node) {
 
-            var name = $(node).find("_id > name").text();
-            var total = parseInt($(node).find("count").text(), 10);
-            var numSuccess = parseInt($(node).find("numSuccess").text());
-            var perc = Math.round(numSuccess / total) * 100;
-            var radial = harlan.widgets.radialProject(result.addItem(name, "").addClass("center").find(".value"), perc);
+            var name = $(node).find("_id > name").text(),
+                total = parseInt($(node).find("count").text(), 10),
+                numSuccess = parseInt($(node).find("numSuccess").text()),
+                perc = Math.round(numSuccess / total) * 100,
+                item = result.addItem(name, ""),
+                radial = controller.interface.widgets.radialProject(item.addClass("center").find(".value"), perc);
+
+            item.find(".name").css({
+                'max-width': '128px',
+                'max-height': '20px',
+                'overflow': 'hidden'
+            });
+
             var datum = {
                 key: name,
                 values: []
             };
 
-            $(node).find("results > node").each(function (idx, result) {
+            $(node).find("results > node").each(function(idx, result) {
                 var jresult = $(result);
                 datum.values.push([
                     parseInt(jresult.find("date").text().match(/\d+$/)[0], 10),
-                    parseInt(jresult.find("responseTime").text(), 10)]);
+                    parseInt(jresult.find("responseTime").text(), 10)
+                ]);
             });
 
             if (perc < 70) {
@@ -51,10 +60,10 @@
             graph.push(datum);
         });
 
-        return result.generate();
+        return result.element();
     };
 
-    harlan.importXMLDocument.register("STATISTICS", "APPLICATION", parserConsultas);
+    controller.importXMLDocument.register("STATISTICS", "APPLICATION", parserConsultas);
 
     applicationStatistics();
 
