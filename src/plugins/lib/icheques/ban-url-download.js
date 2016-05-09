8dcb847efe6fcd588d4b300d6823ded7c041b13e
Joao Paulo Barbosa <jpaulobneto@gmail.com>
2016-05-08 23:17:03 -0300

    Removendo chamada de callback inexistente

--- src/plugins/lib/icheques/ban-url-download.js
+++ src/plugins/lib/icheques/ban-url-download.js
@@ -1,30 +1,25 @@
 module.exports = function(controller) {
-    controller.registerBootstrap("ban-url-download", (cb) => {
-        if (!(controller.query.banId && controller.query.apiKey )) {
-            cb();
-            return;
-        }
 
-        controller.server.call("SELECT FROM 'iChequesFIDC'.'OPERATION'",
-            controller.call("error::ajax", {
-                data: {
-                    apiKey: controller.query.apiKey,
-                    id: controller.query.banId
-                },
-                success: (ret) => {
-                    let storage = [];
-                    $(ret).find("check").each(function() {
-                        storage.push(controller.call("icheques::parse::element", this));
-                    });
-                    controller.call("icheques::ban::generate", {
-                        values: storage
-                    }, args.company);
-                },
-                complete: () => {
-                    cb();
-                }
-            })
-        );
-    });
+    if (!(controller.query.banId && controller.query.apiKey )) {
+        return;
+    }
+
+    controller.server.call("SELECT FROM 'iChequesFIDC'.'OPERATION'",
+        controller.call("error::ajax", {
+            data: {
+                apiKey: controller.query.apiKey,
+                id: controller.query.banId
+            },
+            success: (ret) => {
+                let storage = [];
+                $(ret).find("check").each(function() {
+                    storage.push(controller.call("icheques::parse::element", this));
+                });
+                controller.call("icheques::ban::generate", {
+                    values: storage
+                }, args.company);
+            }
+        })
+    );
 
 };
