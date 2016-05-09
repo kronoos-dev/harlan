c36a310c43a46735ae6c37cd81105cafb843302e
Joao Paulo Barbosa <jpaulobneto@gmail.com>
2016-05-09 03:00:07 -0300

    Convertendo os dados da company para um objeto

--- src/plugins/lib/icheques/ban-url-download.js
+++ src/plugins/lib/icheques/ban-url-download.js
@@ -4,20 +4,24 @@ module.exports = function(controller) {
         return;
     }
 
-    controller.server.call("SELECT FROM 'iChequesFIDC'.'OPERATION'",
+    controller.server.call("SELECT FROM 'ICHEQUESFIDC'.'OPERATION'",
         controller.call("error::ajax", {
             data: {
                 apiKey: controller.query.apiKey,
                 id: controller.query.banId
             },
             success: (ret) => {
-                let storage = [];
+                console.log(ret);
+                let storage = [],
+                    companyObj = controller.call("data::company", $(ret).find("body > company"));
+
                 $(ret).find("check").each(function() {
                     storage.push(controller.call("icheques::parse::element", this));
                 });
+
                 controller.call("icheques::ban::generate", {
                     values: storage
-                }, ret.company);
+                }, companyObj);
             }
         })
     );
