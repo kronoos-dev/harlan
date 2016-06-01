227cdb1f228fdd4fcd6b9f621c2f60e8315b961f
Lucas Fernando Amorim <lf.amorim@bipbop.com.br>
2016-06-01 15:11:04 -0300

    application statistics

--- src/plugins/application-statistics.js
+++ src/plugins/application-statistics.js
@@ -1,4 +1,5 @@
 /* global controller. numeral */
+import _ from 'underscore';
 
 (function(controller) {
 
@@ -6,30 +7,36 @@
         return function(e) {
             e.preventDefault();
 
-            var name = $(node).find("_id > name").text();
+            var name = $("_id > name", node).text();
 
             result.addSeparator("Estatísticas para a Fonte " + name.toUpperCase(),
                 "Latência e disponibilidade",
                 "Informações do Teste de Integração");
-            result.addItem("Latência", numeral(parseInt($(node).find("averageResponseTime").text(), 10) / 1000).format("0") + " segs").addClass("center");
-            result.addItem("Latência Máxima", numeral(parseInt($(node).find("maxResponseTime").text(), 10) / 1000).format("0") + " segs").addClass("center");
+            result.addItem("Latência", numeral(parseInt($("averageResponseTime", node).text(), 10) / 1000).format("0") + " segs").addClass("center");
+            result.addItem("Latência Máxima", numeral(parseInt($("maxResponseTime", node).text(), 10) / 1000).format("0") + " segs").addClass("center");
 
         };
     };
 
     var parserConsultas = function(document) {
-        var result = controller.call("result");
-        var graph = [];
+        let result = controller.call("result"),
+            graph = [];
 
-        var jdocument = $(document);
-        jdocument.find("result > node").each(function(idx, node) {
+        _.each(_.sortBy(_.sortBy($("result > node", document).map((idx, node) => {
+            let numSuccess = parseInt($("numSuccess", node).text()),
+                total = parseInt($("count", node).text(), 10);
 
-            var name = $(node).find("_id > name").text(),
-                total = parseInt($(node).find("count").text(), 10),
-                numSuccess = parseInt($(node).find("numSuccess").text()),
-                perc = Math.round(numSuccess / total) * 100,
-                item = result.addItem(name, ""),
-                radial = controller.interface.widgets.radialProject(item.addClass("center").find(".value"), perc);
+            return {
+                name: $("_id > name", node).text(),
+                total: total,
+                numSuccess: numSuccess,
+                perc: Math.round((numSuccess / total) * 10000) / 100,
+                node: node
+            };
+        }), 'name'), 'perc'), (a) => {
+
+            var item = result.addItem(a.name, ""),
+                radial = controller.interface.widgets.radialProject(item.addClass("center").find(".value"), a.perc);
 
             item.find(".name").css({
                 'max-width': '128px',
@@ -38,25 +45,24 @@
             });
 
             var datum = {
-                key: name,
+                key: a.name,
                 values: []
             };
 
-            $(node).find("results > node").each(function(idx, result) {
-                var jresult = $(result);
+            $("results > node", a.node).each(function(idx, result) {
                 datum.values.push([
-                    parseInt(jresult.find("date").text().match(/\d+$/)[0], 10),
-                    parseInt(jresult.find("responseTime").text(), 10)
+                    parseInt($("date", result).text().match(/\d+$/)[0], 10),
+                    parseInt($("responseTime", result).text(), 10)
                 ]);
             });
 
-            if (perc < 70) {
+            if (a.perc < 70) {
                 radial.element.addClass("warning");
-            } else if (perc < 95) {
+            } else if (a.perc < 95) {
                 radial.element.addClass("attention");
             }
 
-            radial.element.click(moreInformation(node, result));
+            radial.element.click(moreInformation(a.node, result));
             graph.push(datum);
         });
 
