b766a893f168afcaf547b5f34d516eedb1f79af4
Lucas Fernando Amorim <lf.amorim@bipbop.com.br>
2016-06-07 14:37:59 -0300

    disable welcome message

--- src/plugins/lib/kronoos/design/disabled-events.js
+++ src/plugins/lib/kronoos/design/disabled-events.js
@@ -1,3 +1,4 @@
 module.exports = (controller) => {
     controller.unregisterTrigger("authentication::authenticated", "welcomeScreen::authenticated");
+    controller.unregisterTrigger("call::authentication::loggedin", "icheques::welcome");
 };
