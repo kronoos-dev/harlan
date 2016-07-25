3e89dc7d80bf2977e446649b681b4b9efad8f594
Lucas Fernando Amorim <lf.amorim@bipbop.com.br>
2016-07-24 23:10:39 -0300

    feature: melhorando seo do Harlan

--- phantomjs/proxy.js
+++ phantomjs/proxy.js
@@ -1,10 +1,18 @@
-/* global harlan, phantom */
+'use strict';
+
 var webpage = require('webpage');
 var server = require('webserver');
 var system = require('system');
 var waitfor = require('./waitfor.js');
 
-var listening = server.create().listen(8080, function(request, response) {
+var onError = function (response, page) {
+    response.statusCode = 500;
+    response.write(page.content);
+    response.closeGracefully();
+    page.close();
+};
+
+var listening = server.create().listen(8080, function (request, response) {
 
     var page = webpage.create();
     page.settings.loadImages = false;
@@ -13,59 +21,53 @@ var listening = server.create().listen(8080, function(request, response) {
         "Content-Type": "text/html; charset=UTF-8",
         "X-PhantomJS": "true"
     };
-    page.onResourceRequested = function(requestData, request) {
-        if (!/^https?:\/\/(www\.)?harlan\.com\.br/.test(requestData['url']) || requestData.headers['Content-Type'] == 'text/css') {
+    page.onResourceRequested = function (requestData, request) {
+        if (!/^https?:\/\/(www\.)?harlan\.com\.br/.test(requestData.url) || requestData.headers['Content-Type'] === 'text/css') {
             request.abort();
         }
     };
 
-    page.onResourceError = function(resourceError) {};
-    page.onError = function(msg, trace) {};
+    page.onResourceError = function () {};
+    page.onError = function () {};
 
-    page.open("http://www.harlan.com.br" + request.url, "GET", function(status) {
+    page.open("http://www.harlan.com.br" + request.url, "GET", function (status) {
         if (status !== 'success') {
-            response.statusCode = 500;
-            response.write(page.content);
-            response.close();
-            page.close();
-            return;
+            return onError(response, page);
         }
 
-        waitfor(function() {
-            return page.evaluate(function(arg) {
+        waitfor(function () {
+            return page.evaluate(function () {
 
-                if (typeof harlan === "undefined") {
+                if (window.harlan === undefined) {
                     return false;
                 }
 
-                if (typeof pageLoaded === "undefined") {
-                    pageLoaded = false;
+                if (window.pageLoaded === undefined) {
+                    window.pageLoaded = false;
                 }
 
-                if (typeof registered === "undefined") {
-                    harlan.registerTrigger("seo::ready", function(args, callback) {
-                        pageLoaded = true; /* GLOBAL */
+                if (window.registered === undefined) {
+                    window.harlan.registerTrigger("seo::ready", function (args, callback) {
+                        window.pageLoaded = true; /* GLOBAL */
                         callback();
                     }); /* Harlan Global */
-                    registered = true;
+                    window.registered = true;
                 }
 
-                return pageLoaded || $(".result").length > 0;
+                return window.pageLoaded || $(".result").length > 0;
             });
-        }, function() {
+        }, function () {
+            response.setHeader("Cache-Control", "max-age=15552000");
             response.statusCode = 200;
-            return page.evaluate(function () {
+            page.evaluate(function () {
                 $("body").children().not(".app").remove();
             });
             response.write(page.content);
-            response.close();
+            response.closeGracefully();
             page.close();
-        }, function() {
-            response.statusCode = 500;
-            response.write(page.content);
-            response.close();
-            page.close();
-        }, 30000);
+        }, function () {
+            onError(response, page);
+        }, 5000);
     });
 });
 
