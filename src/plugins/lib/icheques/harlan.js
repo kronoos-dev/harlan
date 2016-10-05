1eb496a9f62ea2af25a2bfdc14dfced56d5d4ea6
Helsinque <helsinquedeveloper@gmail.com>
2016-10-04 23:17:03 -0300

    phrase

--- src/plugins/lib/icheques/harlan.js
+++ src/plugins/lib/icheques/harlan.js
@@ -252,7 +252,7 @@ module.exports = function(controller) {
 
                     totalRegistro = parseInt(totalRegistro);
                     if (totalRegistro > 0) {
-                        section[0].find("h3").text(`${currentMessage} Total de registros CCF: ${qteOcorrencias} com total de ocorrências de ${dataUltOcorrencia}`);
+                        section[0].find("h3").text(`${currentMessage} Total de registros CCF: ${qteOcorrencias} com data da última ocorrência: ${dataUltOcorrencia}`);
                         section[0].addClass("warning");
                     }
                 }
