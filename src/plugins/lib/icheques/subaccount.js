74902b94621617c47a77a196345b1458bfef96db
Lucas Fernando Amorim <lf.amorim@bipbop.com.br>
2017-03-10 08:31:34 -0300

    feature: subaccount: removendo necessidade de aceitar contrato

--- src/plugins/lib/icheques/subaccount.js
+++ src/plugins/lib/icheques/subaccount.js
@@ -14,66 +14,49 @@ module.exports = function (controller) {
                 "magicLabel": true,
                 "fields": [
                     [{
-                            "name": "name",
-                            "type": "text",
-                            "placeholder": "Nome do Responsável",
-                            "optional": true,
-                            "labelText": "Nome do Responsável"
-                        },
-                        {
-                            "name": "zipcode",
-                            "type": "text",
-                            "placeholder": "CEP",
-                            "optional": true,
-                            "labelText": "CEP",
-                            "mask": "00000-000"
-                        }],
-                    [{
-                            "name": "email",
-                            "type": "text",
-                            "placeholder": "E-mail",
-                            "optional": false,
-                            "labelText": "E-mail"
-                        },
-                        {
-                            "name": "phone",
-                            "type": "text",
-                            "placeholder": "Telefone (opcional)",
-                            "labelText": "Telefone",
-                            "mask": "(00) 0000-00009",
-                            "optional": true
-                        }],
-                    [{
-                            "name": "password",
-                            "type": "password",
-                            "placeholder": "Senha",
-                            "labelText": "Senha",
-                            "optional": false
-                        },
-                        {
-                            "name": "repeat-password",
-                            "type": "password",
-                            "placeholder": "Repita sua Senha",
-                            "labelText": "Repita sua Senha",
-                            "optional": false
-                        }],
-                    {
-                        "name": "cnpj",
+                        "name": "name",
                         "type": "text",
-                        "placeholder": "CNPJ",
-                        "labelText": "CNPJ",
-                        "mask": "00.000.000/0000-00",
-                        "optional": false,
-                        "maskOptions": {
-                            "reverse": true
-                        }
+                        "placeholder": "Nome do Responsável",
+                        "optional": true,
+                        "labelText": "Nome do Responsável"
                     },
                     {
-                        "name": "accept",
-                        "type": "checkbox",
-                        "labelText": "Aceito o <a href=\"/legal/icheques/MINUTA___CONTRATO___CONTA_CORPORATIVA.pdf\" target=\"_blank\">contrato de utilização</a>.",
+                        "name": "zipcode",
+                        "type": "text",
+                        "placeholder": "CEP",
+                        "optional": true,
+                        "labelText": "CEP",
+                        "mask": "00000-000"
+                    }],
+                    [{
+                        "name": "email",
+                        "type": "text",
+                        "placeholder": "E-mail",
+                        "optional": false,
+                        "labelText": "E-mail"
+                    },
+                    {
+                        "name": "phone",
+                        "type": "text",
+                        "placeholder": "Telefone (opcional)",
+                        "labelText": "Telefone",
+                        "mask": "(00) 0000-00009",
+                        "optional": true
+                    }],
+                    [{
+                        "name": "password",
+                        "type": "password",
+                        "placeholder": "Senha",
+                        "labelText": "Senha",
                         "optional": false
-                    }
+                    },
+                    {
+                        "name": "repeat-password",
+                        "type": "password",
+                        "placeholder": "Repita sua Senha",
+                        "labelText": "Repita sua Senha",
+                        "optional": false
+                    }]
                 ]
             }
         ]
