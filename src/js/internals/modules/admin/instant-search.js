11131249cbaecdeaf36bfea31890644af9eef06f
Lucas Fernando Amorim <lf.amorim@bipbop.com.br>
2017-06-20 16:21:32 -0300

    feature: pesquisa por TAG

--- src/js/internals/modules/admin/instant-search.js
+++ src/js/internals/modules/admin/instant-search.js
@@ -28,6 +28,47 @@ module.exports = function(controller) {
     });
 
 
+
+    controller.registerTrigger("findDatabase::instantSearch", "findCompany::tag", function (args, callback) {
+        callback();
+        let [query, autocompleter] = args;
+        if (!/^tag/i.test(query)) {
+            return;
+        }
+
+        autocompleter.item("Pesquise por uma TAG de Empresa", "Pesquisa de usuários por TAG configurada no cadastro.")
+            .addClass("admin-company admin-new-company")
+            .click((e) => {
+                e.preventDefault();
+                let modal = controller.call("modal");
+                modal.title("Pesquisa por TAG");
+                modal.subtitle("Liste todas as empresas que possuem determinada TAG.");
+                modal.paragraph("Informe a TAG abaixo para que pesquisemos todas as empresas que possuem a TAG informada");
+
+                let form = modal.createForm();
+                let input = form.addInput("tag", "text", "TAG da Empresa").magicLabel();
+
+                form.element().submit((e) => {
+                    e.preventDefault();
+                    modal.close();
+                    controller.serverCommunication.call("SELECT FROM 'BIPBOPCOMPANYS'.'LIST'", {
+                        data: {
+                            tag: input.val()
+                        },
+                        success: function(response) {
+                            $("BPQL > body > company", response).each(function(idx, companyNode) {
+                                var company = $(companyNode),
+                                document = company.children("cnpj").text() || company.children("cpf").text();
+                                controller.call("admin::viewCompany", company);
+                            });
+                        }
+                    });
+                });
+                form.addSubmit('submit', 'Pesquisar');
+                modal.createActions().cancel();
+            });
+    });
+
     controller.registerTrigger("findDatabase::instantSearch", "admin::postPaid", function(args, callback) {
         callback();
         var [argument, autocomplete] = args;
