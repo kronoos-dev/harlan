d5affdf99de129eb07834ce0fca9c3a5a0b27ccd
Lucas Fernando Amorim <lf.amorim@bipbop.com.br>
2017-07-25 17:51:59 -0300

    feature: enviando trigger

--- src/js/internals/modules/admin/send-message.js
+++ src/js/internals/modules/admin/send-message.js
@@ -2,12 +2,67 @@ import each from 'async/each';
 
 module.exports = (controller) => {
 
+    controller.registerCall("admin::trigger", (apiKeys) => {
+        if (!apiKeys.length) {
+            controller.alert({
+                title: "A trigger não pode ser enviada.",
+                subtitle: "É necessário ao menos uma empresa para poder ser disparada uma trigger.",
+                paragraph: "Experimente alterar os filtros de sua pesquisa para que você possa continuar esta operação.",
+            });
+            return;
+        }
+
+        controller.call("form", data => {
+            let modal = controller.call("modal");
+            modal.title("Progresso de Trigger");
+            modal.subtitle("Seu evento está sendo enviado, por favor aguarde.");
+            modal.paragraph("Experimente tomar um café enquanto nossos servidores encaminham as mensagens.");
+
+            let progress = modal.addProgress();
+
+            let sended = 0;
+            each(apiKeys, (apiKey, callback) => $.bipbop("INSERT INTO 'TRIGGER'.'TRIGGER'", apiKey, controller.call("error::ajax", {
+                method: 'POST',
+                data: JSON.stringify([data.trigger, {}, moment().unix() + 10]),
+                contentType: 'application/json; charset=utf-8',
+                dataType: 'json',
+                complete: () => {
+                    progress(++sended / apiKeys.length);
+                    callback();
+                }
+            })), (err) => {
+                toastr.success("O evento foi registrado com sucesso, dentro o sistema realizará as chamadas.",
+                "Mensagem enviada com sucesso");
+                modal.close();
+            });
+        }).configure({
+            "title": "Disparar Evento",
+            "subtitle": "Preencha os campos abaixo para disparar um evento ao cliente.",
+            "paragraph": "Certifique de preencher o campo com um evento válido registrado.",
+            "gamification": "checkPoint",
+            "magicLabel": true,
+            "screens": [{
+                "nextButton": "Enviar",
+                "observations": [
+                    `${apiKeys.length} usuários`
+                ],
+                "fields": [{
+                    "name": 'trigger',
+                    "optional": false,
+                    "type": "text",
+                    "placeholder": "Disparador (trigger)"
+                }]
+            }]
+        });
+    });
+
+
     controller.registerCall("admin::message", (apiKeys) => {
         if (!apiKeys.length) {
             controller.alert({
                 title: "A mensagem não pode ser enviada.",
                 subtitle: "É necessário ao menos uma empresa para poder ser desenvolvida uma mensagem.",
-                paragraph: "Experimente alterar os filtros de sua pesquisa para que você possa escrever uma mensagem.",
+                paragraph: "Experimente alterar os filtros de sua pesquisa para que você possa continuar esta operação.",
             });
             return;
         }
@@ -45,7 +100,11 @@ module.exports = (controller) => {
             "screens": [{
                 "nextButton": "Enviar",
                 "actions": [
-                    ["Editor Markdown", () => window.open("http://dillinger.io/", '_blank').focus()]
+                    ["Editor Markdown", () => window.open("http://dillinger.io/", '_blank').focus()],
+                    ["Trigger", (modal) => {
+                        controller.call("admin::trigger", apiKeys);
+                        modal.close();
+                    }]
                 ],
                 "observations": [
                     `${apiKeys.length} destinarários`
