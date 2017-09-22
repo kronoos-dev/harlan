d21d66b3a1f2a30f03d775ff26111de0ad536b65
Helsinque <helsinquedeveloper@gmail.com>
2017-09-22 16:46:30 -0300

    feature: Parse IEPTB value and data [PROTESTOS]

--- src/js/internals/parsers/ieptb.js
+++ src/js/internals/parsers/ieptb.js
@@ -6,21 +6,26 @@ module.exports = function (controller) {
 
         var result = controller.call("result"),
             jdocument = $(document);
-
 		_.each(jdocument.find("BPQL > body > consulta > conteudo > cartorio"), (element) => {
             result.addSeparator("Protestos em Cartório",
                 $("nome", element).text(),
-                $("endereco", element).text());
+                $("endereco", element).text(),
+                $("valor", element).text(),
+                $("data", element).text());
 
             result.addItem("Protestos", $("protestos", element).text()).addClass("center");
             result.addItem("Telefone", $("telefone", element).text());
             result.addItem("Cidade", $("cidade", element).text());
-            // result.addItem("Data da Atualização", moment($("dt_atualizacao", element).text(), "YYYY-MM-DD")
-            //     .format("DD/MM/YYYY")).addClass("center");
+
+            if (typeof $("data", element).text() == 'string' && $("data", element).text() !== '')
+            {
+                result.addItem("Data do protesto", moment($("data", element).text(), ["YYYY-MM-DD", "DD-MM-YYYY"]).format("DD/MM/YYYY"));
+            }
+            if (typeof $("valor", element).text() == 'string' && $("valor", element).text() !== ''){
+                result.addItem("Valor do protesto", numeral($("valor", element).text().replace(".", ",")).format("$0,0.00"), "valor");
+            }
 		});
-
         return result.element();
-
     };
 
     controller.registerBootstrap("parserIEPTB", function (callback) {
