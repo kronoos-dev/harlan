0d107d726f677543e5319d93a0d43dfd4db24784
Lucas Fernando Amorim <lf.amorim@bipbop.com.br>
2017-11-30 15:25:00 -0200

    hide kronoos

--- src/js/internals/modules/icheques.js
+++ src/js/internals/modules/icheques.js
@@ -12,7 +12,7 @@ module.exports = function (controller) {
 
                 controller.registerTrigger("serverCommunication::websocket::authentication::end", "canUseKronoos", (args, callback) => {
                     callback();
-                    if (!/federalinvest/i.test(controller.confs.user.commercialReference)) {
+                    if (!/federal\s*invest/i.test(controller.confs.user.commercialReference)) {
                         $.getScript("/js/kronoos.js", () => setTimeout(() => controller.trigger("kronoos::init", args), 1000));
                     }
                 });
