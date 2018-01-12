cbbfdb89c9f89f40d860e0272e6b421ce4a7646d
Lucas Fernando Amorim <lf.amorim@bipbop.com.br>
2018-01-12 15:34:32 -0200

    deprecando o dive

--- src/js/app.js
+++ src/js/app.js
@@ -83,7 +83,6 @@ import browserUpdate from 'browser-update';
     require('./internals/modules/instant-search')(harlan);
     require('./internals/modules/tooltip')(harlan);
     require('./internals/modules/icheques')(harlan);
-    require('./internals/modules/dive')(harlan);
     require('./internals/modules/socialprofile')(harlan);
     require('./internals/modules/kronoos')(harlan);
     require('./internals/modules/billing-information')(harlan);
