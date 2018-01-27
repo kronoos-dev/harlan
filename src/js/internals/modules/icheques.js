aaa6b4097beecced2d1a3f8b2e32f581f99ac964
Lucas Fernando Amorim <lf.amorim@bipbop.com.br>
2018-01-27 21:10:48 -0200

    evitando que o kronoos abra varias vezes

--- src/js/internals/modules/icheques.js
+++ src/js/internals/modules/icheques.js
@@ -1,25 +1,23 @@
+import oneTime from 'one-time';
+
 module.exports = controller => {
+    const kronoosCall = oneTime(args => $.getScript('/js/kronoos.js', () => controller.trigger('kronoos::init', args)));
+    if (!controller.confs.icheques.hosts.includes(document.location.hostname)) return;
 
-    if (controller.confs.icheques.hosts.includes(document.location.hostname)) {
-        controller.registerBootstrap('icheques::init::plataform', callback => {
-            controller.confs.disableDive = true;
-            $.getScript('/js/icheques.js', () => {
+    controller.registerBootstrap('icheques::init::plataform', callback => $.getScript('/js/icheques.js', () => {
 
-                if (navigator.userAgent.match(/iPad/i) !== null) {
-                    callback();
-                    return;
-                }
+        if (navigator.userAgent.match(/iPad/i) !== null) {
+            callback();
+            return;
+        }
 
-                controller.registerTrigger('serverCommunication::websocket::authentication::end', 'canUseKronoos', (args, callback) => {
-                    callback();
-                    if (!/federal\s*invest/i.test(controller.confs.user.commercialReference)) {
-                        $.getScript('/js/kronoos.js', () => setTimeout(() => controller.trigger('kronoos::init', args), 1000));
-                    }
-                });
+        callback();
 
-                callback();
-            });
+        controller.registerTrigger('serverCommunication::websocket::authentication::end', 'kronoosPlugin', (args, callbackAuthentication) => {
+            callbackAuthentication();
+            if (/federal\s*invest/i.test(controller.confs.user.commercialReference)) return;
+            kronoosCall(args);
         });
-    }
+    }));
 
 };
