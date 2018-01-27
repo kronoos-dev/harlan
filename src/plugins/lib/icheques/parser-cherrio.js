608fc995cdb656b10026b9450f824e7509f1bfd8
Lucas Fernando Amorim <lf.amorim@bipbop.com.br>
2018-01-27 21:05:03 -0200

    feature: parser em forma de web worker - mais velocidade para grandes contas

---
+++ src/plugins/lib/icheques/parser-cherrio.js
@@ -0,0 +1,43 @@
+import greenlet from 'greenlet';
+
+const databaseInteger = value => value && /^\d+$/.test(value) ? parseInt(value, 10) : null;
+
+module.exports = greenlet(async function (xmlContent) {
+
+    const cheerio = require('cheerio');
+    const $ = cheerio.load(xmlContent, {
+        normalizeWhitespace: true,
+        xmlMode: true
+    });
+
+    const getElement = (element, nodeName) => {
+        const nodeElement = $(nodeName, element);
+        return nodeElement.length ? nodeElement.text() : null;
+    };
+
+    return $.find('check').map((i, node) => ({
+        creation: parseInt(getElement(node, 'creation'), 10),
+        company: getElement(node, 'company'),
+        cmc: getElement(node, 'cmc'),
+        cpf: getElement(node, 'cpf'),
+        cnpj: getElement(node, 'cnpj'),
+        observation: getElement(node, 'observation'),
+        expire: getElement(node, 'expire'),
+        ammount: databaseInteger(getElement(node, 'ammount')),
+        pushId: getElement(node, 'pushId'),
+        situation: getElement(node, 'situation'),
+        display: getElement(node, 'display'),
+        queryStatus: databaseInteger(getElement(node, 'queryStatus')),
+        ocurrenceCode: databaseInteger(getElement(node, 'ocurrenceCode')),
+        ocurrence: getElement(node, 'ocurrence'),
+        operation: databaseInteger(getElement(node, 'operation')),
+        ccf: databaseInteger(getElement(node, 'ccf')),
+        protesto: databaseInteger(getElement(node, 'protesto')),
+        debtCollector: getElement(node, 'debtCollector'),
+        alinea: getElement(node, 'alinea'),
+        lastDebtCollectorMessage: getElement(node, 'lastDebtCollectorMessage'),
+        lastUpdate: databaseInteger(getElement(node, 'lastUpdate')),
+        image: getElement(node, 'image') === '1' ? 1 : 0
+    })).get();
+
+});
