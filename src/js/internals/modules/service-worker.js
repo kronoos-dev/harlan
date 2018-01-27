77ebada3327d5cee0afc00ae39bc0efb0cbf0428
Lucas Fernando Amorim <lf.amorim@bipbop.com.br>
2018-01-27 21:09:16 -0200

    removendo o console.log

--- src/js/internals/modules/service-worker.js
+++ src/js/internals/modules/service-worker.js
@@ -1,15 +1,11 @@
-/* global module, navigator, console */
-
 module.exports = controller => {
 
     controller.registerBootstrap('manifest', callback => {
         callback();
         if ('serviceWorker' in navigator) {
             navigator.serviceWorker.register('/service-worker.js', {scope: './'}).then(() => {
-                console.log('ServiceWorker Running');
             }).catch(err => {
                 // registration failed :(
-                console.log('ServiceWorker registration failed: ', err);
             });
         }
     });
