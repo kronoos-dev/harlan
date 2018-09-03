5b88aeb55513e671e4ddbe9a270c4bea0c3461ff
Lucas Fernando Amorim <lf.amorim@yahoo.com.br>
2018-09-03 13:50:24 -0300

    no-ccf

--- src/js/internals/modules/icheques.js
+++ src/js/internals/modules/icheques.js
@@ -2,6 +2,7 @@ import oneTime from 'one-time';
 
 module.exports = controller => {
     const kronoosCall = oneTime(args => $.getScript('/js/kronoos.js', () => controller.trigger('kronoos::init', args)));
+    const refinCall = oneTime(() => $.getScript('https://www.npmjs.com/package/harlan-icheques-refin'));
     if (!controller.confs.icheques.hosts.includes(document.location.hostname)) return;
 
     controller.registerBootstrap('icheques::init::plataform', callback => $.getScript('/js/icheques.js', () => {
@@ -15,9 +16,9 @@ module.exports = controller => {
 
         controller.registerTrigger('serverCommunication::websocket::authentication::end', 'kronoosPlugin', (args, callbackAuthentication) => {
             callbackAuthentication();
+            if (controller.confs.user.tags.indexOf('no-refin').indexOf() === -1) refinCall();
             if (/federal\s*invest/i.test(controller.confs.user.commercialReference)) return;
             kronoosCall(args);
         });
     }));
-
 };
